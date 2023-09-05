06df5f64570b4611cfe506da50a3e592334b866f
Зендељи Шпреса <shpresa.zendelji@students.finki.ukim.mk>
2023-09-05 03:29:54 +0200

    DomPurify

--- resources/js/Pages/Project/ShowNotes.vue
+++ resources/js/Pages/Project/ShowNotes.vue
@@ -7,14 +7,15 @@
                     <button
                         data-bs-target="#storeNoteModal"
                         data-bs-toggle="modal"
-                        class="btn px-sm-4 text-light  text-nowrap mb-2" :style="'background-color: ' + project.color_hash ">
+                        class="btn px-sm-4 text-light  text-nowrap mb-2"
+                        :style="'background-color: ' + project.color_hash ">
                         Add Note <i class="fa fa-plus ms-2"></i>
                     </button>
                 </div>
             </div>
             <div class="mt-2 mt-lg row g-3">
                 <div v-for="(note,i) in project.notes" :key="i" class="col-12 col-xl-6  align-content-stretch">
-                    <div class="card px-2 p-5 p-sm-5 h-100 position-relative">
+                    <div class="card px-2 p-5 p-sm-5 h-100 position-relative overflow-hidden">
                         <div class="d-flex align-items-center position-absolute top-0 end-0 p-2 text-light ">
                             <i class="fa fa-pencil cursor-pointer p-2 rounded"
                                :style="'background-color: ' + project.color_hash "
@@ -30,7 +31,7 @@
                                :data-bs-route="route('project.notes.destroy',[project,note])">
                             </i>
                         </div>
-                        <div v-html="note.note"></div>
+                        <div v-html="DOMPurify.sanitize(note.note)"></div>
                     </div>
                 </div>
             </div>
@@ -54,6 +55,7 @@ import ProjectLayout from "@/Layouts/ProjectLayout.vue";
 import StoreNoteModal from "@/Modals/Project/ShowNotes/StoreNoteModal.vue";
 import UpdateNoteModal from "@/Modals/Project/ShowNotes/UpdateNoteModal.vue";
 import {inject} from "vue";
+import DOMPurify from "dompurify";
 
 const route = inject("route");
 defineProps({
@@ -64,4 +66,5 @@ defineProps({
 })
 </script>
 
-<style scoped></style>
+<style scoped>
+</style>
